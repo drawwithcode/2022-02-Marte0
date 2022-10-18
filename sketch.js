e4c7b6e545dbb8a3c27d0372f193d1c463955ab0
Marte0 <98111502+Marte0@users.noreply.github.com>
2022-10-18 22:56:09 +0200

    Add files via upload
    agiornamento

--- sketch.js
+++ sketch.js
@@ -1,16 +1,64 @@
-function preload() {
-	// put preload code here
-}
+let particles = [];
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-	// put drawing code here
+  background(0, 35);
+  if (frameCount % 3 == 0) {
+    let fireworkX = random(0 + 100, windowWidth - 100);
+    let fireworkY = random(0 + 100, windowHeight - 100);
+    let fireworkColor = color(
+      random(80, 255),
+      random(80, 255),
+      random(80, 255)
+    );
+
+    for (let i = 0; i < random(100, 1000); i++) {
+      let p = new Particle(fireworkX, fireworkY, fireworkColor);
+      particles.push(p);
+    }
+  }
+  particles.forEach(function (p, index) {
+    p.update();
+    p.show();
+
+    if (alpha(p.color) <= 0) particles.splice(index, 1);
+  });
+}
+
+class Particle {
+  constructor(x, y, color) {
+    this.x = x; //= windowWidth / 2;
+    this.y = y; //= windowHeight / 2;
+    this.vx = random(-3, +3);
+    this.vy = random(-3, +3);
+    this.alpha = 255;
+    this.color = color;
+    this.fade = random(3, 4);
+  }
+
+  show() {
+    strokeWeight(3);
+    this.color.setAlpha(this.alpha);
+    stroke(this.color);
+
+    point(this.x, this.y);
+
+    //fill(255, this.alpha);
+    //ellipse(this.x, this.y, 3);
+  }
+
+  update() {
+    this.x += this.vx;
+    this.vy += 0.07;
+    if (this.vx > 0) this.vx -= 0.03;
+
+    if (this.vx < 0) this.vx += 0.03;
+
+    this.y += this.vy;
+    this.alpha -= this.fade;
+  }
 }
